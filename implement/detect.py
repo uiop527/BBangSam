cb69438d51671139109da029a7a6418e71b045e2
uiop527 <whdud527@naver.com>
2022-03-28 01:16:05 +0900

    extract color_test, check bounding box, typecasting

--- implement/detect.py
+++ implement/detect.py
@@ -9,6 +9,7 @@ import sys
 import time
 import datetime
 import argparse
+import cv2
 
 import torch
 from torch.utils.data import DataLoader
@@ -20,15 +21,15 @@ import matplotlib.patches as patches
 from matplotlib.ticker import NullLocator
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--image_folder', type=str, default='data/facesamples', help='path to dataset')
+parser.add_argument('--image_folder', type=str, default='data/facesamples/', help='path to dataset')
 parser.add_argument('--config_path', type=str, default='config/yolov3face.cfg', help='path to model config file')
 parser.add_argument('--weights_path', type=str, default='checkpoints/face.weights', help='path to weights file')
-parser.add_argument('--class_path', type=str, default='C:/Users/uiop5/design/BBangSam/implement/data/face/face.names', help='path to class label file')
+parser.add_argument('--class_path', type=str, default='data/face/face.names', help='path to class label file')
 parser.add_argument('--conf_thres', type=float, default=0.8, help='object confidence threshold')
 parser.add_argument('--nms_thres', type=float, default=0.2, help='iou thresshold for non-maximum suppression')
 parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
 parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
-parser.add_argument('--img_size', type=int, default=640, help='size of each image dimension')
+parser.add_argument('--img_size', type=int, default=416, help='size of each image dimension')
 parser.add_argument('--use_cuda', type=bool, default=True, help='whether to use cuda if available')
 opt = parser.parse_args()
 print(opt)
@@ -48,7 +49,6 @@ model.eval() # Set in evaluation mode
 
 dataloader = DataLoader(ImageFolder(opt.image_folder, img_size=opt.img_size),
                         batch_size=opt.batch_size, shuffle=False)
-
 classes = load_classes(opt.class_path) # Extracts class labels from file
 print(classes)
 print(opt.class_path)
@@ -62,7 +62,7 @@ prev_time = time.time()
 for batch_i, (img_paths, input_imgs) in enumerate(dataloader):
     # Configure input
     input_imgs = Variable(input_imgs.type(Tensor))
-
+    
     # Get detections
     with torch.no_grad():
         detections = model(input_imgs)
@@ -124,6 +124,32 @@ for img_i, (path, detections) in enumerate(zip(imgs, img_detections)):
             bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2,
                                     edgecolor=color,
                                     facecolor='none')
+            #check bounding box and type casting
+            int_tensor_x=x1.int()
+            position_x = int_tensor_x.item()
+            print(position_x)
+            int_tensor_y=y1.int()
+            position_y = int_tensor_y.item()
+            print(position_y)
+            int_tensor_width=box_w.int()
+            value_w = int_tensor_width.item()
+            print(value_w)
+            int_tensor_height=box_h.int()
+            value_h = int_tensor_height.item()
+            print(value_h)
+            
+            center_x = position_x + int(value_w/2)
+            center_y = position_y + int(value_h/2)
+            
+            #color extract
+            b,g,r = (img[center_y, center_x])
+            print(r)
+            print(g)
+            print(b)
+            
+            plt.imshow(img)
+            plt.show()
+            
             # Add the bbox to the plot
             ax.add_patch(bbox)
             # Add label
